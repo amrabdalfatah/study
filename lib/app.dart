97724141cd45ccf998b68103fe6c4a9ab88f2aeb
amr <amr@amrs-MacBook-Air.local>
2024-08-11 11:58:57 +0300

    add android icon and change title

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:study_academy/routes.dart';
@@ -20,4 +19,4 @@ class MyApp extends StatelessWidget {
       onGenerateRoute: AppRoutes.generateRoutes,
     );
   }
-}
+} 
